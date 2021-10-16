cae7c0f9eb0d42e1dc9375f3917151d269aa0061
Szczepaniak Paweł <pawel.szczepaniak@microsoft.wsei.edu.pl>
2021-10-16 15:36:05 +0200

    Passing action parameters

--- Controllers/Bookscontroller.cs
+++ Controllers/Bookscontroller.cs
@@ -17,5 +17,26 @@ namespace LibApp.Controllers
 
             return View(firstBook);
         }
+
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if(String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
     }
 }
