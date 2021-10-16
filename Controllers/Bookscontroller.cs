d3081be5fc6fde727ab3c897ab3c7526deabf8c1
Szczepaniak Paweł <pawel.szczepaniak@microsoft.wsei.edu.pl>
2021-10-16 15:20:49 +0200

    Added Book MVC

---
+++ Controllers/Bookscontroller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibApp.Models;
+
+namespace LibApp.Controllers
+{
+    public class Bookscontroller : Controller
+    {
+        //GET /Books/Random 
+
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Name = "Eglish Dictionary" };
+
+            return View(firstBook);
+        }
+    }
+}
